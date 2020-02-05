df09e4b560a66b34f81d4464e9438ab26724513a
M-M-Mujtaba <l174144@lhr.nu.edu.pk>
2020-02-05 15:01:27 +0500

    all functions completed , only analysis and formatting left .

--- Algo_Hw1.py
+++ Algo_Hw1.py
@@ -1,4 +1,6 @@
 import  random
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -18,6 +20,21 @@ def insertionSort(A):
 
         A[j] = key
     return A
+def insertionSort_Limit(left, right):
+    temp_counter = 0
+    A = []
+    A.extend(left)
+    A.extend(right)
+    for index in range(1 , len(A)):
+        key = A[index]
+        j = index
+        while key < A[j -1] and j > 0:
+            temp_counter += 1
+            A[j] = A[ j -1]
+            j = j- 1
+
+        A[j] = key
+    return A, temp_counter
 
 
 def merge(left, right):
@@ -41,7 +58,8 @@ def merge(left, right):
     if right:
         result.extend(right[right_idx:])
         counter += len(right[right_idx:])
-    return result, counter;
+    return result, counter
+
 
 def merge_sort(m):
     global counter
@@ -57,7 +75,7 @@ def merge_sort(m):
     left = merge_sort(left)
     right= merge_sort(right)
 
-    A, temp_Counter = merge(left, right)
+    A, temp_Counter = insertionSort_Limit(left, right)
     counter+= temp_Counter
 
     return list(A)
@@ -78,16 +96,16 @@ def sortPloter ():
         #         A.append(x)
         for i in range(N, 0, -1):
             A.append(i)
-        insertionSort(A)
+        A = merge_sort(A)
         result[N-10] = counter
         counter = 0
 
-    plt.plot([i for i in range(10, 1000)], result, 'b',[i for i in range(10, 1000)], [3 * (i * i) // 4 for i in range(10, 1000)], 'r' )
+    plt.plot([i for i in range(10, 1000)], result, 'b',[i for i in range(10, 1000)], [3 * i * i // 4 for i in range(10, 1000)], 'r' )
     plt.ylabel('Step Count')
     plt.xlabel("array size")
     plt.show()
 
-sortPloter()
+#sortPloter()
 
 # N = 100
 # A = []
@@ -106,3 +124,46 @@ sortPloter()
 # print("\n\nSorted array is")
 # print(counter)
 # print(A)
+
+
+def largest_Dec_N2(A):
+    largest_decline = 0
+    while len(A) > 0:
+        x = A.pop()
+        for i in range(0, len(A)):
+            if abs(x - A[i]) > largest_decline:
+                largest_decline = abs(x - A[i])
+
+    print(largest_decline)
+
+def largest_Dec_N(A):
+    min = sys.maxsize * 2 + 1
+    max = -(sys.maxsize * 2 + 1) - 1
+    for i in A:
+        if i < min:
+            min = i
+        elif i > max:
+            max = i
+
+    print(max - min)
+
+def largest_Dec_lgN(A):
+
+    if len(A) == 1:
+        minu = maxu = A[0]
+    else:
+        middle = len(A) // 2
+        left = A[:middle]
+        right = A[middle:]
+        lmin, lmax = largest_Dec_lgN(left)
+        rmin, rmax = largest_Dec_lgN(right)
+        minu = lmin if lmin < rmin else rmin
+        maxu = lmax if lmax > rmax   else rmax
+    return minu, maxu
+
+
+A = [random.randint(3, 25) for i in range(10)]
+print(A)
+min_in , max_in = largest_Dec_lgN(A)
+print(max_in - min_in)
+
