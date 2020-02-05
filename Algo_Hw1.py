966bc880a1e892ca6ffb65008affdfd01b8a1537
M-M-Mujtaba <l174144@lhr.nu.edu.pk>
2020-02-05 10:28:21 +0500

    Modified Merge sort and introduced worst array

--- Algo_Hw1.py
+++ Algo_Hw1.py
@@ -1,11 +1,10 @@
 
-
-
 def insertionSort(A):
     global counter
     for index in range(1 , len(A)):
         key = A[index]
         j = index
+        counter+= 1
         while key < A[ j -1] and j > 0:
             counter += 1
             A[j] = A[ j -1]
@@ -15,64 +14,59 @@ def insertionSort(A):
     return A
 
 
-def Merge(arr, l, r, m):
-    leftArr = arr[l: m+1]
-    rightArr = arr[m+1: r+1]
-
-    # merge sort : copy data back with comparision
-    li = 0  # Initial index of left array
-    ri = 0  # Initial index of rogjt subarray
-    ai = l  # Initial index of sorting array
-
-    while li < len(leftArr) and ri < len(rightArr):
-        if leftArr[li] <= rightArr[ri]:
-            arr[ai] = leftArr[li]
-            li += 1
+def merge(left, right):
+    result = []
+    left_idx, right_idx = 0, 0
+    while left_idx < len(left) and right_idx < len(right):
+        # change the direction of this comparison to change the direction of the sort
+        if left[left_idx] <= right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
         else:
-            arr[ai] = rightArr[ri]
-            ri += 1
-        ai += 1
+            result.append(right[right_idx])
+            right_idx += 1
 
-    # Copy the remaining elements of L[], if there
-    # are any
-    while li <len(leftArr):
-        arr[ai] = leftArr[li]
-        li += 1
-        ai += 1
+    if left:
+        result.extend(left[left_idx:])
+    if right:
+        result.extend(right[right_idx:])
+    return result
 
-    # Copy the remaining elements of R[], if there
-    # are any
-    while ri < len(rightArr):
-        arr[ai] = rightArr[ri]
-        ri += 1
-        ai += 1
+def merge_sort(m):
+    global  counter
+    if len(m) <= 1:
+        return m
 
-counter = 0
-# Recursive function that sorts the input array by breaking it into sub problems and then merging them intelligently
-def mergeSort(A, l, r): # l is the left index and r is the right index
-    global counter
-    if l < r:
-        mid = (l + r) // 2
-        counter += 1
-        mergeSort(A, l, mid)
-        counter += 1
-        mergeSort(A, mid + 1, r)
+    middle = len(m) // 2
+    left = m[:middle]
+    right = m[middle:]
 
-        Merge(A, l, r, mid)
+    left = merge_sort(left)
+    counter+= len(left)
+    right = merge_sort(right)
+    counter+= len(right)
+    return list(merge(left, right))
 
 
 import  random
 
-N = 10000
+counter = 0
+
+
+N = 1024
 A = []
+A_worst = []
 x = 0
 for i in range(0, N):
-    x = random.randint(0, 10000000)
+    x = random.randint(0, 100000)
     if x not in A:
         A.append(x)
-print(A)
-#mergeSort(A, 0, N - 1)
-insertionSort(A)
+
+for i in range(N, 0, -1):
+    A_worst.append(i)
+print(A_worst)
+A_worst = merge_sort(A_worst)
+#insertionSort(A_worst)
 print("\n\nSorted array is")
 print(counter)
-print(A)
+print(A_worst)
