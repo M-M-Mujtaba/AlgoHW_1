9eb77caeb664be38c6bf2ca313fab3e8ca1fc0ff
M-M-Mujtaba <l174144@lhr.nu.edu.pk>
2020-02-05 12:54:05 +0500

    adding a functionality to plot data

--- Algo_Hw1.py
+++ Algo_Hw1.py
@@ -1,3 +1,10 @@
+import  random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
 
 def insertionSort(A):
     global counter
@@ -15,6 +22,7 @@ def insertionSort(A):
 
 
 def merge(left, right):
+    counter = 0
     result = []
     left_idx, right_idx = 0, 0
     while left_idx < len(left) and right_idx < len(right):
@@ -25,15 +33,21 @@ def merge(left, right):
         else:
             result.append(right[right_idx])
             right_idx += 1
+        counter += 1
+
 
     if left:
         result.extend(left[left_idx:])
+        counter += len(left[left_idx:])
     if right:
         result.extend(right[right_idx:])
-    return result
+        counter += len(right[right_idx:])
+    return result, counter;
 
 def merge_sort(m):
-    global  counter
+    global counter
+    temp_Counter = 0
+
     if len(m) <= 1:
         return m
 
@@ -42,31 +56,46 @@ def merge_sort(m):
     right = m[middle:]
 
     left = merge_sort(left)
-    counter+= len(left)
-    right = merge_sort(right)
-    counter+= len(right)
-    return list(merge(left, right))
+    right= merge_sort(right)
+
+    A, temp_Counter = merge(left, right)
+    counter+= temp_Counter
+
+    return list(A)
 
 
-import  random
+
 
 counter = 0
 
+def sortPloter ():
+    global counter
+    result = []
+    for N in range(10, 10001):
+        for i in range(0, N + 1):
+            x = random.randint(0, 100000)
+            if x not in A:
+                A.append(x)
+        A = merge_sort(A)
+        result.append(counter)
+        counter = 0
 
-N = 1024
+
+
+N = 100
 A = []
 A_worst = []
 x = 0
-for i in range(0, N):
+for i in range(0, N+1):
     x = random.randint(0, 100000)
     if x not in A:
         A.append(x)
 
 for i in range(N, 0, -1):
     A_worst.append(i)
-print(A_worst)
-A_worst = merge_sort(A_worst)
+print(A)
+A = merge_sort(A)
 #insertionSort(A_worst)
 print("\n\nSorted array is")
 print(counter)
-print(A_worst)
+print(A)
